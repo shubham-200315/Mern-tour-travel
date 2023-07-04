35fa1a885c78cccca71d361e3d0f78f11c16104a
Shubham Jaiswal <sj428731@gmail.com>
2023-07-04 14:37:36 +0530

    Backend file cleanup

--- routes/users.js
+++ routes/users.js
@@ -8,18 +8,15 @@ import { verifyUser , verifyAdmin } from '../utils/verifyToken.js'
 //Update user
 
 router.put('/:id',verifyUser, updateUser)
-// router.put('/:id', verifyUser, updateUser)
-
 //Delete user
-// router.delete('/:id', deleteUser)
 router.delete('/:id', verifyUser, deleteUser)
 
 //Get single user
-// router.get('/:id', getSingleUser)
+
 router.get('/:id', verifyUser, getSingleUser)
  
 //Get all user
-// router.get('/', getAllUser)
+
 router.get('/', verifyAdmin, getAllUser)
 
 
